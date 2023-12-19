0c2de734a847c23a455079d53552c52836396057
pranay000007 <115402853+pranay000007@users.noreply.github.com>
2023-12-19 12:13:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var header=document.getElementById("header");
+            var ms=document.getElementById("pranay");
+
+            ms.onclick=function(){
+                header.style.backgroundImage="url(https://www.ford.com/content/dam/vdm_ford/live/en_us/ford/nameplate/mustang/2024/collections/equipment/3-2/24_Mustang_07_32.jpg/jcr:content/renditions/cq5dam.web.1440.1440.jpeg)"
+            } 
+        
